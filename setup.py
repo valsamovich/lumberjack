1fec57e5d09e988d5c887e2493097ea85fe2b392
Valery Samovich <valery.y.samovich@disney.com>
2016-11-07 17:38:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
 setup(name='lumberjack',
-      version='0.1.1',
+      version='0.1.2',
       description='Logging library with visual tags',
+      long_description='Library support different tags like [INFO], [WARN], '
+                       'and others. Great for logging with styles!',
       url='https://github.com/vsamov/lumberjack',
       author='Valery Samovich',
       author_email='valerysamovich@gmail.com',
